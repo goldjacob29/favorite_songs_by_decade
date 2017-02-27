cce0cd948c80c4648e9aae5a5f923e14de7d6573
Raghu Betina <raghu@firstdraft.com>
2017-02-27 22:02:46 +0000

    Generate songs resource with starter:resource

---
+++ db/migrate/20170227220246_create_songs.rb
@@ -0,0 +1,14 @@
+class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.integer :artist_id
+      t.integer :genre_id
+      t.integer :decade
+      t.integer :year
+
+      t.timestamps
+
+    end
+  end
+end
